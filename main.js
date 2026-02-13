fa00ab094d4779673a6bbb68b8019ab7d88cb546
Alminario-9R-ay2526 <prinzealminario000@gmail.com>
2026-02-13 13:52:57 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function findArea() {
+    let Base = document.getElementById("bas").value;
+    let Height = document.getElementById("hei").value;
+    let Area = Number(Base) * Number(Height) / 2;
+    document.getElementById("vettel").value = vettel;
+}
